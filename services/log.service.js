065f6adf9e3414b9a1541dc013cc203df977a873
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-11-26 13:39:22 +0300

    add help

---
+++ services/log.service.js
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (err) => {
+    console.log(chalk.bgRed(`Error ${err}`));
+}
+
+const printSuccess = (msg) => {
+    console.log(chalk.bgGreen(` Success ${msg}`));
+}
+
+const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan('Help')}
+        'Без параметров - вывод погоды'
+        -s [CITY] для установки города
+        -h для вывода помощи
+        -t [API_KEY] для сохранения токена
+        `
+    );
+}
+
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
-
+import { printHelp } from './services/log.service.js'
 import { getArgs } from "./helpers/args.js";
 
 const initCLI = () => {
     const args = getArgs(process.argv);
     console.log(args);
+    if (args.h) {
+        printHelp();
+    }
+    if (args.s) {
+        // сохранить город
+    }
+    if (args.t) {
+        // сохранить токен
+    }
+    // вывести погоду
 };
 
 initCLI();
